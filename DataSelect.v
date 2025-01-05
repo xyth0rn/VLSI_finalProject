cf78d3671d6bcca382577eaf0cd41aed848355f2
richard2626 <89074043+richard2626@users.noreply.github.com>
2025-01-05 15:20:15 +0800

    compare dataselect

--- DataSelect.v
+++ DataSelect.v
@@ -1,5 +1,6 @@
 // DataSelecter (register)
 // Routes data between the ALU and other components, acting as a multiplexer.
+// version 1
 module DataSelect (Da, Db, romx, clk, Ctl, dataa, datab);
 	input  [7:0] Da, Db, romx;
 	input  clk, Ctl;
@@ -17,4 +18,24 @@ module DataSelect (Da, Db, romx, clk, Ctl, dataa, datab);
 			1: datab <= romx;
 		endcase
 	end
+endmodule
+
+//version 2
+module DataSelect (
+    input [7:0] Da, Db, romx,
+    input clk, Ctl,
+    output reg [7:0] dataa, datab
+);
+
+    always @ (posedge clk) begin
+        dataa <= Da;
+    end
+
+    always @ (posedge clk) begin
+        case (Ctl)
+            0: datab <= Db;
+            1: datab <= romx;
+        endcase
+    end
+
 endmodule
