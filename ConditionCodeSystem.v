2161951efb48e069b61a8f2cdd0013c49a793c3e
xyth0rn <49625757+xyth0rn@users.noreply.github.com>
2025-01-05 17:33:04 +0800

    Update ConditionCodeSystem.v

--- ConditionCodeSystem.v
+++ ConditionCodeSystem.v
@@ -1,6 +1,6 @@
 module ConditionCodeSystem (cont, aluo, tcnd);
-    input [3:0] cont;   // instruction control line
-    input  [7:0] aluo;  // ALU output
+    input  [3:0] cont;   // instruction control line
+    input  [8:0] aluo;  // ALU output (digit 8 is carry digit
     output tcnd;        // condition control output
 
     // DEBUG: need clock or not?
@@ -11,8 +11,9 @@ module ConditionCodeSystem (cont, aluo, tcnd);
             4'b0011: tcnd = 4'b0001;
             4'b0100: tcnd = (aluo == 0) ? 4'b0001 : 4'b0000;
             4'b0101: tcnd = (aluo != 0) ? 4'b0001 : 4'b0000;
-            4'b0110: tcnd = 
-            4'b0111: tcnd = 
+            4'b0110: tcnd = alou[8];
+            4'b0111: tcnd = !alou[8];
+            default: tcnd = 1'b0;
         endcase
     end
 endmodule
